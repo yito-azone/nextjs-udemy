a48fb885b1e1c45d3309b2553eb357942498562c
y.ito <yukitakaito1230@gmail.com>
2023-12-30 22:09:29 +0900

    完成系

---
+++ nextjs-microblog/pages/index.js
@@ -0,0 +1,54 @@
+import Head from "next/head";
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import styles from "@/styles/Home.module.css";
+import Link from "next/link";
+import Layout, { siteTitle } from "@/components/Layout";
+import utilStyle from "../styles/utils.module.css";
+import { getPostsData } from "../lib/post";
+
+const inter = Inter({ subsets: ["latin"] });
+
+// SSGの場合
+export async function getStaticProps() {
+  const allPostsData = getPostsData();
+  console.log(allPostsData);
+
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+}
+
+export default function Home({ allPostsData }) {
+  return (
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <setion className={utilStyle.headingMd}>
+        <p>
+          私はフルスタックエンジニアです/Udemyの講師として活動しています/好きな言語はJavascriptです
+        </p>
+      </setion>
+      <section>
+        <h2>📝エンジニアのブログ</h2>
+        <div className={styles.grid}>
+          {allPostsData.map(({ id, title, date, thumbnail }) => (
+            <article key={id}>
+              <Link href={`/posts/${id}`}>
+                <img src={`${thumbnail}`} className={styles.thumbnailImage} />
+              </Link>
+              <Link href="/">
+                <div className={utilStyle.boldText}>{title}</div>
+              </Link>
+              <br />
+              <small className={utilStyle.lightText}>{date}</small>
+            </article>
+          ))}
+        </div>
+      </section>
+    </Layout>
+  );
+}
